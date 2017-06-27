a48742ab0a08f689bf47edc8bccdd2781e23b048
Gustavo Fulton <ferna194@msu.edu>
2017-06-27 17:31:20 -0400

    Revert "Added cart functionality"
    This reverts commit eb41cac17d0e843acdfcec72e48baeae5c7d9808.

--- client/src/pages/cart/cart.ts
+++ client/src/pages/cart/cart.ts
@@ -8,20 +8,10 @@ import { CheckOutPage } from '../checkout/checkout';
 })
 export class CartPage {
   currentCart = <any>[];
-  total = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidEnter() {
     this.currentCart = window.localStorage.getItem('cart');
     this.currentCart = JSON.parse(this.currentCart);
-    this.total = 0;
-    if (this.currentCart.length > 0) {
-      for (let item of this.currentCart) {
-        this.total += item.price;
-      }
-    }
     console.log(this.currentCart);
   }
 
