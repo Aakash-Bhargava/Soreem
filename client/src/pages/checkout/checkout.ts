f46750d1ac0b4d294b3d6309e92e5f7f1bff7876
Gustavo Fulton <ferna194@msu.edu>
2017-06-29 16:29:38 -0400

    Added credit card scanner

--- client/src/pages/checkout/checkout.ts
+++ client/src/pages/checkout/checkout.ts
@@ -3,20 +3,34 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Http, Headers } from '@angular/http';
 
 import { Stripe } from '@ionic-native/stripe';
+import { CardIO } from '@ionic-native/card-io';
 
 @Component({
   selector: 'page-checkout',
   templateUrl: 'checkout.html'
 })
 export class CheckOutPage {
+  cardImage = "assets/Images/credit-card.png";
+
   cardinfo: any = {
+    type: '',
     number: '',
     expMonth: '',
     expYear: '',
-    cvc: ''
+    cvv: ''
   }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, private stripe: Stripe) {
+  card = {
+    cardType: '',
+    cardNumber: '',
+    redactedCardNumber: '',
+    expiryMonth: null,
+    expiryYear: null,
+    cvv: '',
+    postalCode: ''
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public cardIO: CardIO, private stripe: Stripe) {
     this.stripe.setPublishableKey('pk_live_lBf9W8G3n0UCNUUIEHmZqNx5');
 
   }
@@ -34,6 +48,38 @@ export class CheckOutPage {
     })
   })
 }
+
+scanCard() {
+   this.cardIO.canScan()
+     .then(
+     (res: boolean) => {
+       if (res) {
+         let options = {
+            scanExpiry: true,
+            useCardIOLogo: true,
+            scanInstructions: 'Please position your card inside the frame',
+            keepApplicationTheme: true,
+            requireCCV: true,
+            requireExpiry: true,
+            requirePostalCode: true
+         };
+         this.cardIO.scan(options).then(response => {
+           console.log("Scan complete");
+           console.log(response);
+          //  let { cardType, cardNumber, redactedCardNumber, expiryMonth, expiryYear, cvv, postalCode } = response;
+           this.card = {
+             cardType: response['cardType'],
+             cardNumber: response['cardNumber'],
+             redactedCardNumber: response['redactedCardNumber'],
+             expiryMonth: response['expiryMonth'],
+             expiryYear: response['expiryYear'],
+             cvv: response['cvv'],
+             postalCode: response['postalCode']
+           }
+         });
+       }
+     });
+ }
 
 
 
